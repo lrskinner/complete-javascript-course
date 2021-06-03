78b6e184f4f27f1a9f5878c85e91442b8db45709
Leah Skinner <leahrskins@gmail.com>
2021-06-03 15:40:01 +1200

    complete challenge 1

---
+++ 01-Fundamentals-Part-1/starter/challenge1.js
@@ -0,0 +1,37 @@
+// let heightMark = 1.69;
+// let massMark = 78;
+
+// let heightJohn = 1.95;
+// let massJohn = 92;
+
+// let bmiMark = massMark / heightMark** 2;
+// let bmiJohn = massJohn / heightJohn** 2;
+
+// console.log(bmiMark);
+// console.log(bmiJohn);
+
+// let markHigherBmi = bmiMark > bmiJohn;
+
+// console.log(markHigherBmi);
+
+let heightMark = 1.88;
+let massMark = 95;
+
+let heightJohn = 1.76;
+let massJohn = 85;
+
+let bmiMark = massMark / heightMark** 2;
+let bmiJohn = massJohn / heightJohn** 2;
+
+console.log(bmiMark);
+console.log(bmiJohn);
+
+let markHigherBmi = bmiMark > bmiJohn;
+
+
+console.log(markHigherBmi);
+
+
+
+
+
