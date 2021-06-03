3b68afb5f6bbbbd79dcae286c50074d4f695784c
Leah Skinner <leahrskins@gmail.com>
2021-06-04 11:52:38 +1200

    complete functions lecture assignment

--- 02-Fundamentals-Part-2/assignments.js
+++ 02-Fundamentals-Part-2/assignments.js
@@ -1,3 +1,14 @@
 'use strict';
 
+//FUNCTIONS
+
+function describeCountry(country, population, capitalCity) {
+    const message = `${country} has ${population} million people and its capital city is ${capitalCity}`;
+    return message;
+}
+
+let describeFin = describeCountry('Finland', 6, 'Helsinki');
+let describeNZ = describeCountry('New Zealand', 5, 'Wellington');
+let describeAus = describeCountry('Australia', 28, 'Canberra');
+console.log(describeFin, '\n', describeNZ, '\n', describeAus);
 
