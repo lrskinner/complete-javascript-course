b3664157cb2c372747ab421e56bf5c60a3455133
Leah Skinner <leahrskins@gmail.com>
2021-06-04 13:16:50 +1200

    complete arrow function assignment

--- 02-Fundamentals-Part-2/assignments.js
+++ 02-Fundamentals-Part-2/assignments.js
@@ -10,5 +10,37 @@ function describeCountry(country, population, capitalCity) {
 let describeFin = describeCountry('Finland', 6, 'Helsinki');
 let describeNZ = describeCountry('New Zealand', 5, 'Wellington');
 let describeAus = describeCountry('Australia', 28, 'Canberra');
-console.log(describeFin, '\n', describeNZ, '\n', describeAus);
+// console.log(describeFin, '\n', describeNZ, '\n', describeAus);
 
+
+//FUNCTION DECLARATIONS VS EXPRESSIONS
+
+//Function declaration
+function perOfWorld1(population) {
+    return (population / 7900) * 100;
+} 
+
+//Function expression
+const perOfWorld2 = function (population) {
+    return (population / 7900) * 100;
+}
+
+// const nzPop = perOfWorld1(5);
+// const chinaPop = perOfWorld1(1411);
+
+// console.log(nzPop, chinaPop);
+
+// const portPop = perOfWorld2(10);
+// const usaPop = perOfWorld2(332);
+
+// console.log(portPop, usaPop);
+
+
+//ARROW FUNCTIONS
+
+const perOfWorld3 = population => (population / 7900) * 100;
+
+const nzPop = perOfWorld3(5);
+const chinaPop = perOfWorld3(1411);
+
+console.log(nzPop, chinaPop);
