bcec10a879ae4286d985a9d1f10e5307f781ce88
Leah Skinner <leahrskins@gmail.com>
2021-06-25 14:39:36 +1200

    converted repeated code to function

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -7,7 +7,7 @@ const btnsOpenModal = document.querySelectorAll('.show-modal')
 
 console.log(btnsOpenModal);
 
-//cycles through each modal 
+//cycles through each modal and removes the hidden class on the selected modal when clicked
 for (let i = 0; i < btnsOpenModal.length; i++) 
   btnsOpenModal[i].addEventListener('click', () => {
     // console.log('BUTTON CLICKED!');
@@ -15,11 +15,29 @@ for (let i = 0; i < btnsOpenModal.length; i++)
     overlay.classList.remove('hidden');
   });
 
-    btnCloseModal.addEventListener('click', () => {
-    // console.log('X btn clicked');
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
-  })
+    //this targets the close-modal 'x' button and adds the hidden class back to the element when either the x or anywhere on the overlay is clicked 
+
+    // btnCloseModal.addEventListener('click', () => {
+    // modal.classList.add('hidden');
+    // overlay.classList.add('hidden');
+    
+    // overlay.addEventListener('click', () => {
+    // modal.classList.add('hidden');
+    // overlay.classList.add('hidden');
+    
+
+    //refactored by converting the shared code to a function so it is available to be used multiple times with less code
+    const closeModal = () => {
+      modal.classList.add('hidden');
+      overlay.classList.add('hidden');
+    }
+
+    btnCloseModal.addEventListener('click', closeModal)
+    overlay.addEventListener('click', closeModal)
+      
+
+
+ 
 
 
 
