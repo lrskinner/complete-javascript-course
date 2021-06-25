d7e863d4113f1d17faf468af8a41cff300461b72
Leah Skinner <leahrskins@gmail.com>
2021-06-25 14:26:17 +1200

    ability to open and close modals

--- 06-Modal/script.js
+++ 06-Modal/script.js
@@ -7,7 +7,19 @@ const btnsOpenModal = document.querySelectorAll('.show-modal')
 
 console.log(btnsOpenModal);
 
+//cycles through each modal 
 for (let i = 0; i < btnsOpenModal.length; i++) 
-  console.log(btnsOpenModal[i].textContent);
+  btnsOpenModal[i].addEventListener('click', () => {
+    // console.log('BUTTON CLICKED!');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
 
-  
+    btnCloseModal.addEventListener('click', () => {
+    // console.log('X btn clicked');
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+  })
+
+
+
