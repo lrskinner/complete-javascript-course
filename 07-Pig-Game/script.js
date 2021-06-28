ed4f96473699f22ae3d5bff3e1412050683eb996
Leah Skinner <leahrskins@gmail.com>
2021-06-28 13:10:29 +1200

    ability to toggle between players

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -7,8 +7,12 @@ const score0Ele = document.querySelector('#score--0');
 const score1Ele = document.getElementById('score--1');
 
 //variables for current score for each player
-const current0Ele = document.getElementById('current--0')
-const current1Ele = document.getElementById('current--1')
+let current0Ele = document.getElementById('current--0')
+let current1Ele = document.getElementById('current--1')
+
+const player1 = document.querySelector('.player--0')
+const player2 = document.querySelector('.player--1')
+
 
 //create varibles for the dice and roll buttons so they are easily accessible, and less code needs to be repeated
 const diceEle = document.querySelector('.dice');
@@ -23,12 +27,13 @@ score1Ele.textContent = 0;
 diceEle.classList.add('hidden')
 
 let currentScore = 0;
+//create a variable for the active player, 0 being player 1, 1 being player 2
+let activePlayer = 0;
 
 //dice roll functionality
 btnRoll.addEventListener('click', function () {
   //generate random dice roll
   const dice = Math.trunc(Math.random() * 6) + 1;
-  console.log(dice);
   //display dice
   diceEle.classList.remove('hidden');
   diceEle.src = `dice-${dice}.png`;
@@ -37,10 +42,27 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     //add current dice number to score
     currentScore += dice;
-    current0Ele.textContent = currentScore //change later to be dynamic depending on which player is selected
-    
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+
   //if not true (player rolls a 1) switch player
   } else {
-
+    //first try
+    ////player1.classList.remove('player--active');
+    ////player2.classList.add('player--active');
+    ////current0Ele = 0;
+    
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    //toggle will remove the following class if it's set and add it if it's not.
+    //applying it to both elements will ensure only one will have it set
+    player1.classList.toggle('player--active')
+    player2.classList.toggle('player--active')
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
   }
 });
+
+btnHold.addEventListener('click', () => {
+  document.getElementById(`score--${activePlayer}`).textContent = currentScore;
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+})
